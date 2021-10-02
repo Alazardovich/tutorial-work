e022a37b19501755ec5c8fb12abdac1f2763c3b1
Artem <lazarazuy@gmail.com>
2021-10-02 23:01:05 +0200

    parcel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import addPassword from "./module-1";
+console.log(addPassword(false));
+
+// import {
+//     x as value, 
+//     y
+// }from "./module-2";
+
+// console.log(value ,y);
+
+import * as moduleAll from "./module-2";
+console.table(moduleAll);
---
+++ src/module-2.js
@@ -0,0 +1,15 @@
+export const fletchAllUsers = () => {
+    console.log('fletchAllUsers');
+};
+
+export const fletchUserForId = () => {
+    console.log('fletchUserForId');
+};
+
+export const updateUser = () => {
+    console.log('updateUser');
+};
+
+// export default {fletchAllUsers, fletchUserForId, updateUser }
+export const x = 5;
+export const y = "qew";
